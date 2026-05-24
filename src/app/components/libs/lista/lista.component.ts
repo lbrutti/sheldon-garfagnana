2fcd0566ce43fa80a165fbc7877f4d8be787cff8
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-05-24 09:34:41 +0200

    aggiunto riga totali

--- src/app/components/libs/lista/lista.component.ts
+++ src/app/components/libs/lista/lista.component.ts
@@ -14,11 +14,11 @@ import {DataInterface, FilterOptionInterface} from '../../../interfaces';
 import {DynamicFilterComponent} from '../dynamic-filter/dynamic-filter.component';
 import {getReducedValue} from '../../../utils';
 import {MatList, MatListItem} from '@angular/material/list';
-import {CurrencyPipe} from '@angular/common';
+import {CurrencyPipe, DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'sheldon-list',
-  imports: [CardComponent, DynamicFilterComponent, MatList, MatListItem, CurrencyPipe],
+  imports: [CardComponent, DynamicFilterComponent, MatList, MatListItem, CurrencyPipe, DecimalPipe],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss',
 })
@@ -52,6 +52,7 @@ export default class ListaComponent {
   }
 
   udm = input<string | null>('€');
+  totaleValore = computed<number>(() => this.filteredData().map(d => d.valore).reduce((a, b) => a + b, 0));
 
 
 }
