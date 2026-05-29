5d76d4a8d9c73b451239481021b64f1c6b46ac74
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-05-29 15:49:39 +0200

    Auto-generated commit

---
+++ chunk-PDKKJUFT.js
@@ -0,0 +1,2 @@
+import{Bb as o,Cb as i,_b as a,eb as n}from"./chunk-AOSHXJYT.js";var s=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["sheldon-map-view"]],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),a(1,"map-view works!"),i())},encapsulation:2})}return e})();export{s as default};
+//# sourceMappingURL=chunk-PDKKJUFT.js.map
