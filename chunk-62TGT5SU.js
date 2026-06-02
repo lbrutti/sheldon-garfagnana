cc424696d77e00c9c9ca778f6ec9647cf973aed9
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-06-02 11:52:11 +0200

    Auto-generated commit

---
+++ chunk-62TGT5SU.js
@@ -0,0 +1,2 @@
+import{Db as n,Eb as i,ac as a,gb as o}from"./chunk-7QHWMYHH.js";var d=(()=>{class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["sheldon-public-stories"]],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),a(1,"data-stories works!"),i())},encapsulation:2})}return t})();export{d as default};
+//# sourceMappingURL=chunk-62TGT5SU.js.map
---
+++ chunk-JXYN3OW6.js
@@ -0,0 +1,2 @@
+import{Db as o,Eb as i,ac as a,gb as n}from"./chunk-7QHWMYHH.js";var s=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["sheldon-map-view"]],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),a(1,"map-view works!"),i())},encapsulation:2})}return e})();export{s as default};
+//# sourceMappingURL=chunk-JXYN3OW6.js.map
