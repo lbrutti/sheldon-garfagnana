ffc470ffa2dcaf81f29be02a324e62ea6770ea40
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-06-03 17:18:12 +0200

    Auto-generated commit

--- chunk-FC4GZXBP.js
+++
@@ -1,2 +0,0 @@
-import{Eb as o,Fb as i,bc as a,hb as n}from"./chunk-JUERSZLF.js";var s=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["sheldon-map-view"]],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),a(1,"map-view works!"),i())},encapsulation:2})}return e})();export{s as default};
-//# sourceMappingURL=chunk-FC4GZXBP.js.map
