76966ab19966255888fbcc4bf300d8ae1094328c
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-06-07 11:17:16 +0200

    aggiunti start e stop point più casuali al gradiente

--- src/app/utils/color.utils.ts
+++ src/app/utils/color.utils.ts
@@ -1,4 +1,5 @@
 import {normalizzaStringa} from './data.utils';
+import {endWith} from 'rxjs';
 
 export function resolveColorVariable(variableName: string): string {
   const style = getComputedStyle(document.body);
@@ -6,8 +7,10 @@ export function resolveColorVariable(variableName: string): string {
 }
 
 export function getRandomGradient(categoria: string, rotation: string = '0deg', minStop = 10, maxStop = 90) {
-  const stopPoint = Math.floor(Math.random() * (maxStop - minStop + 1) + minStop) + '%';
+  const startPoint = Math.floor(Math.random() * (100 ));
+
+  const endPoint = Math.floor(Math.random() * (100));
   const categorieStandard = ['ambiente', 'cultura', 'mobilita', 'sicurezza', 'sociale', 'economia'];
   const catNorm = normalizzaStringa(categoria) || categorieStandard[Math.floor(Math.random() * categorieStandard.length)];
-  return `linear-gradient(${rotation}, var(--color-gradient-${catNorm}-start), var(--color-gradient-${catNorm}-end) ${stopPoint})`
+  return `linear-gradient(${rotation}, var(--color-gradient-${catNorm}-start) ${Math.min(startPoint, endPoint)}%, var(--color-gradient-${catNorm}-end) ${Math.max(startPoint, endPoint)}%)`
 }
