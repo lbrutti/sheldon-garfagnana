3a833a481648e241ae5930bf0de0f0002e8350ed
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2026-06-30 17:11:51 +0200

    set back stories routes for dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,14 +12,14 @@ export const routes: Routes = [
     path: 'dashboard',
     component: Dashboard
   },
-  // {
-  //   path: 'stories',
-  //   loadComponent: () => import('./components/pages/data-stories/data-stories')
-  // },
-  // {
-  //   path: 'stories/:id',
-  //   loadComponent: () => import('./components/pages/data-story/data-story')
-  // },
+  {
+    path: 'stories',
+    loadComponent: () => import('./components/pages/data-stories/data-stories')
+  },
+  {
+    path: 'stories/:id',
+    loadComponent: () => import('./components/pages/data-story/data-story')
+  },
   {
     path: 'map',
     loadComponent: () => import('./components/pages/map-view/map-view')
